3fc109f1213e60f2a8d4ab3d342465981a5a525c
doszke <jakub.siembida@interia.pl>
2020-06-14 21:29:42 +0200

    first commit

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import math
 import numpy as np
 from matplotlib import pyplot as plt
-
-
-class Lambda:
-
-    def __init__(self, km, k_cat, c_e0):
-        self.km = km
-        self.k_cat = k_cat
-        self.c_e0 = c_e0
-
-    def value(self):
-        return self.k_cat*self.c_e0/self.km
 
 
 class Equation:
@@ -25,32 +13,86 @@ class Equation:
         self.v = v
 
     def calculate_c(self, delta_u, t):
-        denominator = self.n * self.delta_h * self.lmbda.value() * self.s * self.r_eff * self.v * np.exp(-self.lmbda.value() * t)
+        denominator = self.n * self.delta_h * self.lmbda * self.s * self.r_eff * self.v * np.exp(-self.lmbda * t)
         return delta_u / denominator
 
     def calculate_delta_u(self, c, t):
-        denominator = self.n * self.delta_h * self.lmbda.value() * self.s * self.r_eff * self.v * np.exp(
-            -self.lmbda.value() * t)
+        denominator = self.n * self.delta_h * self.lmbda * self.s * self.r_eff * self.v * np.exp(
+            -self.lmbda * t)
         return c * denominator
 
 
-if __name__ == '__main__':
-    lmbda = Lambda(km=1.1*10**-6, k_cat=8.0/150.0*10**-15, c_e0=1)
-    eq = Equation(s=35*10**-6, r_eff=666.66, delta_h=-30*10**3, lmbda=lmbda, v=1000*10**-6, n=200)
-    print(lmbda.value())
-    time = np.zeros([100]) # 100 miejsc
-    rt = 5 # time(100)
-    dt = rt/100 # dt
-    for x in range(100):
-        time[x] = dt*x
-    print(time)
-    c = np.zeros([6])
-    for x in range(6):
-        c[x] = 49*10**-6 + x * 1*10**-6
-    for x in range(6):
-        data = np.zeros([100])
-        for y in range(100):            # 6 serii po 100 pkt
-            data[y] = eq.calculate_delta_u(c[x], time[y])
-        plt.plot(time, data)
-        print(data[99]-data[0])
+def delta_u_from_lambda(eq):
+    before_lmbda = eq.lmbda
+    c = 50 * 10 ** -6  # 50 nmol/ ml
+    base_lambda = 0.05
+    t = 10
+    lmbda = np.zeros([11])
+    for x in range(11):
+        lmbda[x] = 0.5 * base_lambda + 0.1 * base_lambda * x
+    deltau = np.zeros([11])
+    for x in range(11):
+        eq.lmbda = lmbda[x]
+        deltau[x] = eq.calculate_delta_u(c, t)
+    plt.plot(lmbda, deltau)
+    plt.xlabel("lambda [1/s]")
+    plt.ylabel("ΔU [V]")
+    eq.lmbda = before_lmbda
     plt.show()
+
+
+def delta_u_from_c(eq):
+    dt = 0.5
+    t = np.zeros(11)
+    for x in range(0, 11):
+        t[x] = (x) * dt
+    c = np.zeros([50])
+    for y in range(0, 11):
+        for x in range(50):
+            c[x] = 1 * 10 ** -6 * x
+        data = eq.calculate_delta_u(c, t[y])
+        plt.plot(c, data)
+    plt.xlabel("c [M]")
+    plt.ylabel("ΔU [V]")
+    plt.show()
+
+
+def delta_u_from_r_eff(eq):
+    before_reff = eq.r_eff
+    c = 50 * 10 ** -6  # 50 nmol/ ml
+    t = 10
+    base_reff = 666.66
+    r_eff = np.zeros([11])
+    for x in range(11):
+        r_eff[x] = 0.5 * base_reff + 0.1 * base_reff * x
+    deltau = np.zeros([11])
+    for x in range(11):
+        eq.r_eff = r_eff[x]
+        deltau[x] = eq.calculate_delta_u(c, t)
+    plt.plot(r_eff, deltau)
+    plt.xlabel("effective thermal resistance [K/W]")
+    plt.ylabel("ΔU [V]")
+    eq.r_eff = before_reff
+    plt.show()
+
+
+def delta_u_from_cm(eq):
+    c = 50 * 10 ** -6  # 50 nmol/ ml
+    t = 10
+    c_vec = np.zeros([11])
+    for x in range(11):
+        c_vec[x] = 0.5 * c + 0.1 * c * x
+    deltau = np.zeros([11])
+    for x in range(11):
+        deltau[x] = eq.calculate_delta_u(c_vec[x], t)
+    plt.plot(c_vec, deltau)
+    plt.xlabel("concentration [M]")
+    plt.ylabel("ΔU [V]")
+    plt.show()
+
+
+if __name__ == '__main__':
+    eq = Equation(s=35*10**-6, r_eff=666.66, delta_h=-30*10**3, lmbda=0.05, v=1000*10**-6, n=200)
+    delta_u_from_cm(eq)
+    delta_u_from_lambda(eq)
+    delta_u_from_r_eff(eq)
